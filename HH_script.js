09549163de2365f551d25622f47be5f17f7579c3
HenryHaataja <162910410+HenryHaataja@users.noreply.github.com>
2024-03-13 16:48:17 +0200

    Create HH_script.js

---
+++ HH_script.js
@@ -0,0 +1,10 @@
+
+function toggleDarkMode() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+}
+
+function toggleLanguage() {
+	var element = document.body;
+	element.classList.toggle("en");
+}
